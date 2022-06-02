5404b9b51c60b75aad8025257ff53f7f5c312177
Jesslyn-Chai <jeffreychai118@gmail.com>
2022-06-02 11:18:12 +0800

    search funtion

--- src/BST.java
+++ src/BST.java
@@ -15,4 +15,40 @@ public class BST {
     // root node of the BST
     TreeNode root;
 
+    // search functions
+    // searchTree() function
+    public void searchTree(int data) {
+        if (root == null) {
+            System.out.println("There is no tree!");
+        } else {
+            System.out.println("Searching for key " + data + ".");
+            root = searchNode(data, root);
+        }
+    }
+
+    // searchNode() function
+    public TreeNode searchNode(int data, TreeNode prevRoot) {
+        if (data == prevRoot.data) {
+            System.out.println("Found key " + data + ".");
+        } else if (data < prevRoot.data) {
+            System.out.println(data + " < " + prevRoot.data + ". Searching for left subtree.");
+            searchNode(data, prevRoot.left);
+        } else if (data > prevRoot.data ) {
+            System.out.println(data + " > " + prevRoot.data + ". Searching for right subtree.");
+            searchNode(data, prevRoot.right);
+        } else if (prevRoot == null) {
+            System.out.println("Cannot find key " + data + " in this tree.");
+        }
+
+        return prevRoot;
+    }
+
+    // traversal functions
+    // preorder
+
+
+    // inorder
+    // postorder
+
+
 }
