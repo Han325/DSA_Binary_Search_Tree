e85e148b91a192f54c8f0113e1af3468cf963e27
Eu Yu Han <yuhan1791@gmail.com>
2022-06-02 10:35:03 +0800

    Set tree structure

---
+++ src/BST.java
@@ -0,0 +1,18 @@
+public class BST {
+
+    public class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    // root node of the BST
+    TreeNode root;
+
+}
